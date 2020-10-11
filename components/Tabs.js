c95672c81af755d7dc0852dd630c904f4d93b312
xandre9-git <jmjames673@gmail.com>
2020-10-11 11:34:17 -0400

    completed tabs portion of sprint

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,43 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+// container to hook onto
+const divTopics = document.querySelector('div.topics')
+
+// axios data to get and use
+const dataTopics = axios.get('https://lambda-times-api.herokuapp.com/topics')
+    .then(res => {
+        let topics = res.data.topics // set topic to get array data for list of trending topics
+
+        res.data.topics.forEach(res => {  // create for each loop that takes the array
+            let tab = tabCreator(res)  // creates a variable called tab that uses tabcreator function
+            divTopics.appendChild(tab)  // append data (topic) to parentnode(divTopics) and then repeat above
+        })
+
+    })
+    .catch(err => {  // err function in case promise is unfulfilled
+        console.log(err)
+    })
+
+// div tab creator function
+const tabCreator = (arr) => {  // function that creates div tabs for trending topic
+    const divTab = document.createElement('div')  // create var that creates a a div element
+    divTab.classList.add('tab') // assign that div element a class of tab
+
+    const topicTextNode = document.createTextNode(arr)  // create a textnode for the topics since they are coming from an array
+    divTab.appendChild(topicTextNode) // append the textnode to the div element
+
+    return divTab // return the function to complete
+}
+
+
+console.log(divTopics)
+
+
+
+
+
+
+
+
